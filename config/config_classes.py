8a7f168684f348cf44e280ee2a7212b5e5c64726
AKira Ryo <106382882+AkiraRy@users.noreply.github.com>
2024-11-10 16:17:53 +0100

    Changed BaseSettings class to inherit from pydantics BaseModel for fields validation

--- config/config_classes.py
+++ config/config_classes.py
@@ -4,14 +4,10 @@ from typing import Dict, Optional
 from pathlib import Path
 import yaml
 from .settings import DEFAULT_SETTINGS, ensure_directory_exists, logger, LLM_SETTINGS
+from pydantic import BaseModel
 
 
-@dataclass
-class BaseSettings(ABC):
-    @abstractmethod
-    def validate(self):
-        pass
-
+class BaseSettings(BaseModel):
     def load_from_yaml(self, filepath: str):
         if Path(filepath).exists():
             with open(filepath, 'r') as f:
@@ -21,22 +17,22 @@ class BaseSettings(ABC):
 
     def save_to_yaml(self, filepath: str):
         with open(filepath, 'w') as f:
-            yaml.dump(asdict(self), f)
+            yaml.dump(self.dict(), f)
 
-    def __repr__(self):
-        return yaml.dump(asdict(self))
+    def __str__(self):
+        fields_str = ', '.join(f"{key}={value!r}" for key, value in self.dict().items())
+        return f"{self.__class__.__name__}({fields_str})"
 
 
-@dataclass
 class TelegramSettings(BaseSettings):
-    def validate(self):
-        errors = []
-        if not self.bot_username:
-            errors.append("Bot username is required.")
-        if self.creator_id <= 0:
-            errors.append("Creator ID must be a positive integer.")
-        if errors:
-            raise ValueError(f"Validation errors in TelegramSettings: {', '.join(errors)}")
+    # def validate(self):
+    #     errors = []
+    #     if not self.bot_username:
+    #         errors.append("Bot username is required.")
+    #     if self.creator_id <= 0:
+    #         errors.append("Creator ID must be a positive integer.")
+    #     if errors:
+    #         raise ValueError(f"Validation errors in TelegramSettings: {', '.join(errors)}")
 
     bot_username: str = ""
     bot_nickname: str = ""  # used for storing in the vectordb
@@ -45,16 +41,15 @@ class TelegramSettings(BaseSettings):
     sticker_path: str = ""
 
 
-@dataclass
 class WeaviateSettings(BaseSettings):
-    def validate(self):
-        errors = []
-        if not self.class_name:
-            errors.append("Class name is required.")
-        if not (0 < self.http_port < 65536):
-            errors.append("Port must be a valid number between 1 and 65535.")
-        if errors:
-            raise ValueError(f"Validation errors in WeaviateSettings: {', '.join(errors)}")
+    # def validate(self):
+    #     errors = []
+    #     if not self.class_name:
+    #         errors.append("Class name is required.")
+    #     if not (0 < self.http_port < 65536):
+    #         errors.append("Port must be a valid number between 1 and 65535.")
+    #     if errors:
+    #         raise ValueError(f"Validation errors in WeaviateSettings: {', '.join(errors)}")
 
     author_name: str
     class_name: str = "MemoryK"  # for testing i will use an already created class
@@ -73,23 +68,21 @@ class WeaviateSettings(BaseSettings):
 
 
 @dataclass
-class PluginSettings:  # no idea currently how to add validation here.
+class PluginSettings:  # no idea currently how to make this work. in future fix
     plugin_name: str
     plugin_config: Dict[str, str] = field(default_factory=dict)
 
 
-@dataclass
 class DiscordSettings(BaseSettings):
-    def validate(self):
-        raise NotImplemented
+    # def validate(self):
+    #     raise NotImplemented
 
     bot_name: str = ""
 
 
-@dataclass
 class LLMSettings(BaseSettings):
-    model_name: str
-    model_file: str
+    llm_model_name: str  # Pydantic safe
+    llm_model_file: str
     verbose: bool
     cuda: int
     chat_format: str
@@ -107,32 +100,31 @@ class LLMSettings(BaseSettings):
     seed: int | None = None
     stop: str | list[str] | None = field(default_factory=list)
 
-    def validate(self):
-        pass
+    # def validate(self):
+    #     pass
 
 
-@dataclass
 class Config(BaseSettings):
     telegram: Optional[TelegramSettings] = None
     discord: Optional[DiscordSettings] = None
     weaviate: Optional[WeaviateSettings] = None
-    plugins: Dict[str, PluginSettings] = field(default_factory=dict)
+    # plugins: Dict[str, PluginSettings] = field(default_factory=dict)
     llm: Optional[LLMSettings] = None
     llm_type: str = None
     persona: str = "default_persona"
 
-    def validate(self):
-        if not self.weaviate:
-            raise ValueError("Weaviate must exists")
-        if not (self.telegram or self.discord):
-            raise ValueError("At least one of Telegram, Discord, or Weaviate settings must be provided.")
-
-        if self.telegram:
-            self.telegram.validate()
-        if self.discord:
-            self.discord.validate()
-        if self.weaviate:
-            self.weaviate.validate()
+    # def validate(self):
+    #     if not self.weaviate:
+    #         raise ValueError("Weaviate must exists")
+    #     if not (self.telegram or self.discord):
+    #         raise ValueError("At least one of Telegram, Discord, or Weaviate settings must be provided.")
+    #
+    #     if self.telegram:
+    #         self.telegram.validate()
+    #     if self.discord:
+    #         self.discord.validate()
+    #     if self.weaviate:
+    #         self.weaviate.validate()
 
 
 class SettingsManager:
@@ -153,14 +145,14 @@ class SettingsManager:
                     self.config.discord = DiscordSettings(**data['discord'])
                 if 'weaviate' in data:
                     self.config.weaviate = WeaviateSettings(**data['weaviate'])
-                if 'plugins' in data:
-                    for name, settings in data['plugins'].items():
-                        self.config.plugins[name] = PluginSettings(plugin_name=name, plugin_config=settings)
+                # if 'plugins' in data:
+                #     for name, settings in data['plugins'].items():
+                #         self.config.plugins[name] = PluginSettings(plugin_name=name, plugin_config=settings)
 
                 self.config.llm_type = data.get('llm_type', 'default')
                 self.config.persona = data.get("persona", "default_persona")
                 self.load_llm_settings()
-                self.config.validate()
+                # self.config.validate()
                 logger.info("Settings loaded successfully.")
 
             except (FileNotFoundError, yaml.YAMLError) as e:
@@ -181,8 +173,8 @@ class SettingsManager:
                     'telegram': lambda: TelegramSettings(**data['telegram']) if 'telegram' in data else None,
                     'discord': lambda: DiscordSettings(**data['discord']) if 'discord' in data else None,
                     'weaviate': lambda: WeaviateSettings(**data['weaviate']) if 'weaviate' in data else None,
-                    'plugins': lambda: {name: PluginSettings(plugin_name=name, plugin_config=settings)
-                                        for name, settings in data['plugins'].items()} if 'plugins' in data else None,
+                    # 'plugins': lambda: {name: PluginSettings(plugin_name=name, plugin_config=settings)
+                    #                     for name, settings in data['plugins'].items()} if 'plugins' in data else None,
                     'llm': lambda: self.load_llm_settings() if 'llm_type' in data else None
                 }
 
@@ -234,7 +226,7 @@ class SettingsManager:
             with open(llm_settings_path, 'r') as f:
                 llm_data = yaml.safe_load(f)
                 self.config.llm = LLMSettings(**llm_data)
-                self.config.llm.validate()
+                # self.config.llm.validate()
                 logger.info(f"LLM settings for {self.config.llm_type} loaded successfully.")
         except (FileNotFoundError, yaml.YAMLError) as e:
             logger.error(f"Error loading LLM settings from {llm_settings_path}: {e}")
