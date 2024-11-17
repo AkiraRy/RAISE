dacff565b1ee92b25564ebdfd2d61bb41852592c
AKira Ryo <106382882+AkiraRy@users.noreply.github.com>
2024-11-17 19:45:16 +0100

    Fixed logging to file. Permission denied and wrong file path

--- config/settings.py
+++ config/settings.py
@@ -9,6 +9,8 @@ import logging
 load_dotenv()
 
 BASE_DIR = Path(__file__).parent.parent
+LOGS_DIR = BASE_DIR / 'logs'
+LOGS_FILE = LOGS_DIR / 'infos.log'
 
 # Settings
 CONFIG_DIR = BASE_DIR / 'config'
@@ -53,7 +55,7 @@ LOGGING_CONFIG = {
         "file": {
             'level': "INFO",
             'class': "logging.FileHandler",
-            'filename': "../logs/infos.log",
+            'filename': f"{LOGS_FILE}",
             'mode': "w",
             'formatter': "verbose"
         },
@@ -74,9 +76,6 @@ def get_logger(name="programming"):
     return logging.getLogger(name)
 
 
-logger = get_logger()
-
-
 def ensure_directory_exists(path: Path):
     if not path.exists():
         try:
@@ -95,3 +94,6 @@ ensure_directory_exists(BACKUP_DIR)
 ensure_directory_exists(MODEL_DIR)
 ensure_directory_exists(PROMPT_TEMPLATES_DIR)
 ensure_directory_exists(PERSONA_DIR)
+ensure_directory_exists(LOGS_DIR)
+
+logger = get_logger()
