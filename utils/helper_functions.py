707a9c2fc045f93382358179ca4ad6aacdf659c7
AKira Ryo <106382882+AkiraRy@users.noreply.github.com>
2025-01-26 12:23:07 +0100

    Ability to run directly any fastapi server in subprocess. Gracefull shutdown

--- utils/helper_functions.py
+++ utils/helper_functions.py
@@ -1,12 +1,19 @@
+import os
+import signal
 import subprocess
 import sys
 from config import logger
 
 
-def start_server_handler():
+def start_server_handler(server_module: str, port: int):
     try:
         process = subprocess.Popen(
-            ["python", "-m", "server_handler"],
+            [
+                "uvicorn",
+                server_module,
+                "--host", "127.0.0.1",
+                "--port", str(port)
+            ],
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             text=True
@@ -23,13 +30,20 @@ def start_server_handler():
         sys.exit(1)
 
     except Exception as e:
-        logger.error(f"Error starting server_handler: {e}")
+        logger.error(f"Error starting uvicorn server: {e}")
         sys.exit(1)
 
 
-def terminate_process(process, process_name=""):
-    if process.poll() is None:
-        process.terminate()
-        logger.info(f"Terminated {process_name}.")
+def terminate_process(process, process_name="", timeout=10):
+    if process.poll() is None:  # Check if the process is still running
+        try:
+            os.kill(process.pid, signal.SIGINT)
+            process.wait(timeout=timeout)
+            logger.info(f"Gracefully terminated {process_name}.")
+        except Exception as e:
+            logger.error(f"Failed to terminate {process_name} gracefully: {e}. Forcing termination.")
+            process.terminate()
+            process.wait()
+            logger.info(f"Forcefully terminated {process_name}.")
     else:
         logger.info(f"Process {process_name} was not running.")
