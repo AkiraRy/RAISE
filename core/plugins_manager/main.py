1f92c657a8840f670eb2a5606786b19c01523cea
AKira Ryo <106382882+AkiraRy@users.noreply.github.com>
2025-02-21 19:01:17 +0100

    Ability to load plugins using name

--- core/plugins_manager/main.py
+++ core/plugins_manager/main.py
@@ -27,7 +27,55 @@ class PluginManager(metaclass=Singleton):
         self.plugins = {}
         self.plugins_metadata = discover_plugins()
 
-    def new_plugins_discovered(self) -> dict | bool:
+    def unload_plugin(self, name):
+        # do i delete it from metadata?
+        # Probably not, good thing for caching purposes. Otherwise,  on each unload i would need to discover new plugins again
+        pass
+
+    def _get_path_for_plugin(self, plugin_name): # add logging
+        # first is metadata
+        plugin_path = self.plugins_metadata.get(plugin_name, None)
+
+        if plugin_path:
+            return plugin_path
+
+        # second one is from discover plugins
+        new_plugins = self.discover_new_plugins()
+
+        if not new_plugins:
+            return False  # couldnt discover plugin.
+
+        # check plugin in newly discovered plugins.
+
+        plugin_path_in_discovered = new_plugins.get(plugin_name, None)
+
+        if not plugin_path_in_discovered:
+            return False
+
+        return plugin_path_in_discovered
+
+    def load_plugin(self, name) -> bool:
+        # add positive logging
+        if name in self.plugins:
+            logger.info(f"[PluginManager/load_plugin(name)] Plugin {name} is already loaded")
+            return False
+
+        plugin_path = self._get_path_for_plugin(name)
+
+        if not plugin_path:
+            logger.warning(f"[PluginManager/load_plugin(name)] Plugin {name} doesnt exist.")
+            return False
+
+        raw_config = read_config(plugin_path, name)
+        entry_path, class_name, config_class_name = get_plugin_classes(raw_config, plugin_path, name)
+
+        if not entry_path:
+            logger.warning(f"[PluginManager/load_plugin(name)] Failed to get classes for {name} plugin.")
+            return False
+
+        return self._load_plugin(name, entry_path=entry_path, class_name=class_name, config_class_name=config_class_name, raw_config=raw_config)
+
+    def discover_new_plugins(self) -> dict | bool:
         all_plugins = discover_plugins()
         new_plugins_metadata = {k: all_plugins[k] for k in all_plugins if k not in self.plugins_metadata}
         return False if not new_plugins_metadata else new_plugins_metadata
@@ -37,7 +85,7 @@ class PluginManager(metaclass=Singleton):
             logger.warning(f"[PluginManager/load_plugins] Plugins directory '{PLUGIN_BASE_DIR}' not found.")
             return
 
-        for plugin_name, plugin_path in self.plugins_metadata.items():
+        for plugin_name, plugin_path in self.plugins_metadata.items():  # check if loaded?
             raw_config = read_config(plugin_path, plugin_name)
 
             entry_path, class_name, config_class_name = get_plugin_classes(raw_config, plugin_path, plugin_name)
@@ -54,7 +102,7 @@ class PluginManager(metaclass=Singleton):
         if self.plugins.get(plugin_name) is not None:
             logger.debug(
                 f"[PluginManager/_load_plugins] Plugin {plugin_name} is already loaded.")
-            return
+            return False
 
         logger.debug(f"[PluginManager/_load_plugins] Entering with those args: {plugin_name=}, {entry_path=}, {class_name=}, {config_class_name=}, {raw_config=}")
         module_name = f"{plugin_name}_plugin"
@@ -64,7 +112,7 @@ class PluginManager(metaclass=Singleton):
 
         if not hasattr(module, class_name):
             logger.error(f"[PluginManager/_load_plugins] Plugin {plugin_name}: Class '{class_name}' not found.")
-            return
+            return False
 
         PluginClass = getattr(module, class_name)
 
@@ -75,12 +123,13 @@ class PluginManager(metaclass=Singleton):
                 config = ConfigClass(**raw_config)
             except TypeError as e:
                 logger.error(f"[PluginManager/_load_plugins] Failed to initialize config for {plugin_name}: {e}")
-                return
+                return False
         else:
             config = raw_config
 
         self.plugins[plugin_name] = PluginClass(logger, config)
         logger.info(f"[PluginManager/_load_plugins] Loaded plugin: {plugin_name}")
+        return True
 
     def get_plugin(self, name):
         return self.plugins.get(name)
@@ -130,10 +179,3 @@ def read_config(plugin_path, plugin_name):
     logger.debug(f"[read_config] Loading config: {config_path}\tfor plugin: {plugin_name}")
     with open(config_path, "r") as f:
         return yaml.safe_load(f) if config_path.endswith(".yaml") else json.load(f)
-
-
-# function to discover names of plugins in the plugin folder
-# - populate the metadata dictionary using that.
-# use that load plugins
-# add access to that function through fastapi.
-# make a helper function to discover if there is a new plugin? instead of doing that logic on server side
